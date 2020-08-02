33a4cd6f673f6448be909c643256d9bbf19e3edb
Onwuzor Victor <onwuzorvictor@outlook.com>
2020-08-02 14:57:26 +0100

    create dasboard component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  {
+    path: 'dashboard',
+    loadChildren: './dashboard/dashboard.module#DashboardModule',
+  },
+  { path: '**', redirectTo: 'dashboard' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
