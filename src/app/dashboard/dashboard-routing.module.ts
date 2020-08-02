d061c5658afa511dac0bef110c66395c9e464bc5
Onwuzor Victor <onwuzorvictor@outlook.com>
2020-08-02 15:56:17 +0100

    add both home and user to admin dasboard route

--- src/app/dashboard/dashboard-routing.module.ts
+++ src/app/dashboard/dashboard-routing.module.ts
@@ -2,12 +2,17 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { DashboardComponent } from './dashboard.component';
+import { HomeComponent } from './components/home/home.component';
+import { UserComponent } from './components/user/user.component';
 
 const routes: Routes = [
   {
     path: '',
     component: DashboardComponent,
-    children: []
+    children: [
+      { path: '', component: HomeComponent },
+      { path: 'users', component: UserComponent },
+    ]
 }
 ];
 
