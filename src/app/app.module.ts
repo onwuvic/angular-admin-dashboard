6d5aaa43de47ecc13e2bb9dd9c728b2996ff2427
Onwuzor Victor <onwuzorvictor@outlook.com>
2020-08-02 14:51:04 +0100

    create side nav and header nav component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './shared/nav/nav.component';
+import { SideNavComponent } from './shared/side-nav/side-nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    SideNavComponent
   ],
   imports: [
     BrowserModule,
