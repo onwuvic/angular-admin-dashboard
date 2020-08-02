0ca72eaf47c8d4d7f28bc08e04b5da9f1370e9ac
Onwuzor Victor <onwuzorvictor@outlook.com>
2020-08-02 15:12:32 +0100

    add shared module to app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavComponent } from './shared/nav/nav.component';
-import { SideNavComponent } from './shared/side-nav/side-nav.component';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NavComponent,
-    SideNavComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
