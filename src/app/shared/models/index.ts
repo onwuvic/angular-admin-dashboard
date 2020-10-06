f8ff76ad4cab0f785d9bf5f381ae3daa9dadce2d
Onwuzor Victor <onwuzorvictor@outlook.com>
2020-10-06 04:41:10 +0100

    white space rule

--- src/app/shared/models/index.ts
+++ src/app/shared/models/index.ts
@@ -7,9 +7,9 @@ declare interface RouteInfo {
 
 export const ROUTES: RouteInfo[] = [
   { path: '/dashboard', title: 'Dashboard',  icon: 'pe-7s-graph', class: '' },
-  { path: '/dashboard/users', title: 'User Profile',  icon:'pe-7s-user', class: '' },
-  { path: '/dashboard/tables', title: 'Table List',  icon:'pe-7s-note2', class: '' },
-  { path: '/dashboard/maps', title: 'Maps',  icon:'pe-7s-map-marker', class: '' },
-  { path: '/dashboard/notifications', title: 'Notifications',  icon:'pe-7s-bell', class: '' },
-  { path: '/dashboard/settings', title: 'Settings',  icon:'pe-7s-config', class: 'active-pro' },
+  { path: '/dashboard/users', title: 'User Profile',  icon: 'pe-7s-user', class: '' },
+  { path: '/dashboard/tables', title: 'Table List',  icon: 'pe-7s-note2', class: '' },
+  { path: '/dashboard/maps', title: 'Maps',  icon: 'pe-7s-map-marker', class: '' },
+  { path: '/dashboard/notifications', title: 'Notifications',  icon: 'pe-7s-bell', class: '' },
+  { path: '/dashboard/settings', title: 'Settings',  icon: 'pe-7s-config', class: 'active-pro' },
 ];
